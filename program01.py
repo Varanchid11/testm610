9319857f0fca2c1a0a181e251655d360b7527013
Varanchid11 <467133@wsk.ac.th>
2026-06-24 07:54:44 +0000

    program01.py

---
+++ program01.py
@@ -0,0 +1,7 @@
+print("Hello world")
+print("-----------")
+num1=float(input("erter number1:"))
+num2=float(input("erter number2:"))
+sum=num1+num2
+print("total is",sum)
+print("Author by Varanchid")
